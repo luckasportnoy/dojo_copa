71fa142954a33ef7b915b1d2272ac104691ca029
edsonlb <ti_edson@yahoo.com.br>
2014-05-23 21:08:26 -0300

    index

--- copa/views.py
+++ copa/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def listaJogo(request):
+	return render(request, 'copa.html')
--- programa/urls.py
+++ programa/urls.py
@@ -6,5 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^jogos/$', ),
+    url(r'^$','copa.views.index'),
 )
