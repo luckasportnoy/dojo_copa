c8690438890f17b9ca9e60cc32086020d1514ca6
edsonlb <ti_edson@yahoo.com.br>
2014-05-23 22:02:22 -0300

    Pronto

--- copa/models.py
+++ copa/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class Jogos(models.model):
-	time1 = models.charfild(db_index=True, maxlength='50', null=False)
-	time2 = models.charfild(db_index=True, maxlength='50', null=False)
-	score1 = models.charfild(db_index=True, maxlength='50', null=False)
-	score2 = models.charfild(db_index=True, maxlength='50', null=False)
+class Jogos(models.Model):
+	time1 = models.CharField(db_index=True, max_length='50', null=False)
+	time2 = models.CharField(db_index=True, max_length='50', null=False)
+	score1 = models.CharField(db_index=True, max_length='50', null=False)
+	score2 = models.CharField(db_index=True, max_length='50', null=False)
--- copa/views.py
+++ copa/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from copa.models import Jogos
 
 def listaJogo(request):
-	return render(request, 'copa.html')
+	jogo=Jogos.objects.all()
+	return render(request, 'copa.html', {'jogo':jogo})
+
+def salvar(request):
+	time1 = request.POST.get('time1')
+	score1 = request.POST.get('score1')
+	time2 = request.POST.get('time2')
+	score2 = request.POST.get('score2')
+
+	jogo = Jogos(time1=time1, score2=score2, time2=time2, score1=score1)
+	jogo.save()
+	return listaJogo(request)
+
+
+
